e6980c3452e8a39b12248e7a74322a068c39aab4
LuisCallejaCaballero <tepeleno2002@hotmail.es>
2025-05-19 15:19:16 +0200

    yepa

---
+++ entregables/UseAuth/UseAuth/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+import {DummyLogin, AuthProvider} from './components/AuthProvider'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <AuthProvider>
+
+      <DummyLogin></DummyLogin>
+
+    </AuthProvider>
+
+
+    
+
+    </>
+  )
+}
+
+export default App
+
